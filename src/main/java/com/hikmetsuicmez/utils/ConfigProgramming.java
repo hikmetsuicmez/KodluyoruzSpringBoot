a198dc7c6f2e6e3e94ea8ac5f30c027894d23b90
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2022-12-26 19:22:02 +0300

    DevTools Aktif etmek

---
+++ src/main/java/com/hikmetsuicmez/utils/ConfigProgramming.java
@@ -0,0 +1,8 @@
+package com.hikmetsuicmez.utils;
+
+public class ConfigProgramming {
+
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically (OK clicked)(Ctrl+Alt+s)
+//    file ==> Advanced Settings ==> Allow Auto-make to start even (OK clicked)
+}
