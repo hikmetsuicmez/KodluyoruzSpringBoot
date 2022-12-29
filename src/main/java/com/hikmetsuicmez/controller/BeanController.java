c9b0039a13974d339b907afb8d70e86f9527d703
Hikmet Suiçmez <suicmezhikmet1@gmail.com>
2022-12-29 12:10:54 +0300

    @PostConstruct

---
+++ src/main/java/com/hikmetsuicmez/controller/BeanController.java
@@ -0,0 +1,37 @@
+package com.hikmetsuicmez.controller;
+
+import com.hikmetsuicmez.bean.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+@Controller
+public class BeanController {
+    @Autowired
+    BeanConfig beanConfig;
+
+    // http://localhost:8080/bean/beanDto
+    @GetMapping("/bean/beanDto") // url
+    @ResponseBody // html olmadan direkt ekranda göster
+    public String getBeanDto() {
+        return beanConfig.beanDto()+"";
+    }
+
+    @Autowired
+    Logger LOG;
+
+    @PostConstruct
+    public void init() {
+        LOG.info("Log info cağrıldı.");
+    }
+
+    public static void main(String[] args) {
+        BeanController beanController = new BeanController();
+        System.out.println(beanController);
+    }
+
+}
